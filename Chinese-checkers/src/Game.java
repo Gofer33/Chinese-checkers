fe8b0516d40fc4eff3cbb31c3e9e6c18b1e2eae1
Gofer33 <34025866+Gofer33@users.noreply.github.com>
2017-12-28 00:25:18 +0100

    Fixing bugs, cleaning code

--- Chinese-checkers/src/Game.java
+++ Chinese-checkers/src/Game.java
@@ -5,21 +5,37 @@ import java.util.ArrayList;
  * Created by Damian Borek on 27/11/2017.
  */
 public class Game {
-
+    int AIPlayers;
+    int humanPlayers;
     Board board = new Board();
+    String tableName;
     ArrayList<Player> players;
     int playersNumber;
     TurnManager turn;
     boolean isStarted;
     boolean roomFull;
 
-    public Game (int humanPlayers, int AIPlayers)
+    public Game (int humanPlayers, int AIPlayers, String tableName)
     {
+        this.AIPlayers = AIPlayers;
+        this.humanPlayers = humanPlayers;
+        this.tableName = tableName;
         players = new ArrayList<Player>();
         this.turn = new TurnManager(players);
         playersNumber = humanPlayers + AIPlayers;
         isStarted = false;
         roomFull = false;
+        addBots(AIPlayers);
+    }
+
+    private void addBots(int ammount)
+    {
+        String botName = "Bot";
+        for(int i=0;i<ammount ; i++) {
+            botName = botName + GamesManager.getInstance().games.size() +Integer.toString(i);
+            AIPlayer bot = new AIPlayer(botName, this.tableName);
+            this.addPlayer(bot);
+        }
     }
     public void addPlayer(Player player)
     {
@@ -33,13 +49,6 @@ public class Game {
         players.remove(player);
     }
 
-    public void play ()
-    {
-        while(true)
-        {
-            turn.next();
-        }
-    }
 
     public void start ()
     {
@@ -47,56 +56,56 @@ public class Game {
     if(playersNumber == 2)
     {
         players.get(0).setMark('Y');
-        //players.get(0).setOppositeMark('R');
         players.get(1).setMark('R');
-       // players.get(1).setOppositeMark('Y');
     }
     else if(playersNumber == 3)
     {
         players.get(0).setMark('Y');
-        //players.get(0).setOppositeMark('R');
         players.get(1).setMark('P');
-        //players.get(1).setOppositeMark('L');
         players.get(2).setMark('B');
-        //players.get(2).setOppositeMark('G');
     }
     else if(playersNumber == 4)
     {
         players.get(0).setMark('G');
-        //players.get(0).setOppositeMark('B');
         players.get(1).setMark('P');
-        //players.get(1).setOppositeMark('L');
         players.get(2).setMark('L');
-        //players.get(2).setOppositeMark('P');
         players.get(3).setMark('B');
-        //players.get(3).setOppositeMark('G');
     }
     else if(playersNumber == 6)
     {
         players.get(0).setMark('Y');
-       // players.get(0).setOppositeMark('R');
         players.get(1).setMark('G');
-        //players.get(1).setOppositeMark('B');
         players.get(2).setMark('P');
-        //players.get(2).setOppositeMark('L');
         players.get(3).setMark('R');
-        //players.get(3).setOppositeMark('Y');
         players.get(4).setMark('B');
-       // players.get(4).setOppositeMark('G');
         players.get(5).setMark('L');
-       // players.get(5).setOppositeMark('P');
     }
         this.isStarted = true;
-    }
-
-    public boolean isStarted()
-    {
-        return isStarted;
     }
 
     public boolean isFull()
     {
         return roomFull;
+    }
+
+    public String getTableName()
+    {
+        return this.tableName;
+    }
+
+    public int getAIPlayers()
+    {
+        return this.AIPlayers;
+    }
+
+    public int getHumanPlayers()
+    {
+        return this.humanPlayers;
+    }
+
+    public int getPlayersNumber()
+    {
+        return this.playersNumber;
     }
 
 }
